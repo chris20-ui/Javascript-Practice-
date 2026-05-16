69ea4833dcd29b0315d263fd5f0801f11c39e441
chris20-ui <christianbariasaguilar@gmail.com>
2026-05-16 17:34:30 +0800

    Add files via upload

---
+++ Try And Catch.js
@@ -0,0 +1,21 @@
+try{
+const dividend = Number(window.prompt("Enter a dividend"))
+const divisor =  Number (window.prompt("Enter a divisor"))
+
+
+if (divisor==0){
+    throw new Error("You can't divide zero")
+}
+
+if(isNaN(dividend)|| isNaN(divisor)){
+
+    throw new Error("value must be a number")
+}
+const result= dividend/divisor
+console.log(result)
+}
+
+catch(error){
+console.error(error)
+}
+console.log("hahahahah it works")
