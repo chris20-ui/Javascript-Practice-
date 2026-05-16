47f290117b6ff20b1e68b594206c3c648917e3ec
chris20-ui <christianbariasaguilar@gmail.com>
2026-05-16 17:35:44 +0800

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,12 @@
+try{
+console.log(x)
+}
+catch(error){
+console.error(error)
+}
+
+finally{
+    console.log("Even it has reference error it will  able to read ")
+}
+
+console.log("that's all for today")
